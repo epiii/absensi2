1e005da4aa5f4684dd852acfa5ae3c054bf5e1ae
epiii <elfri.app@gmail.com>
2020-01-13 21:00:47 +0700

    con

--- konfig/connection.php
+++ konfig/connection.php
@@ -1,13 +1,4 @@
 <?php
-// $dbhost = 'https://srv64.niagahoster.com/'; 
-// $dbhost = 'https://srv64.niagahoster.com:2083'; 
-// $dbhost = '193.168.194.1'; 
-// $dbhost = 'http://elfrilancer.com'; 
-
-// $dbhost = 'srv64.niagahoster.com';
-// $dbuser = 'u6726487_db_uid';
-// $dbname = 'u6726487_db_uid';
-// $password = '9kali9=81db';
 
 $dbhost = 'localhost'; 
 $dbuser = 'root';
